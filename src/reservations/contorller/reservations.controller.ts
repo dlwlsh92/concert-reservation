20f96b0efe28761ecde8b3befbe7fcc195a0938f
dlwlsh92 <ljinho92@gmail.com>
2024-04-05 01:38:53 +0900

    fix: 스펠링 오타 수정

--- src/reservations/contorller/reservations.controller.ts
+++ src/reservations/contorller/reservations.controller.ts
@@ -1,61 +1,54 @@
-import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 
-@Controller('reservations')
+@Controller("reservations")
 export class ReservationsController {
+  @Post("token")
+  async createToken(
+    @Body("userId") userId: number,
+    @Body("concertId") concertId: number
+  ) {
+    return "token";
+  }
 
-    @Post('token')
-    async createToken(
-        @Body('userId') userId: number,
-        @Body('concertId') concertId: number
-    ) {
-        return 'token'
-    }
+  @Get("token/validation")
+  async validateToken(@Query("reservationToken") reservationToken: string) {
+    return {
+      status: "available",
+      waitingTime: null,
+    };
+  }
 
-    @Get('token/validation')
-    async validateToken(
-        @Query('reservationToken') reservationToken: string
-    ) {
-        return {
-            status: 'available',
-            waitingTime: null,
-        }
-    }
+  @Get(":concertId/available-dates")
+  async getAvailableDates(@Param("concertId") concertId: number) {
+    return [
+      {
+        concertEventId: 1,
+        startDate: new Date(),
+        maxSeatCapacity: 50,
+        currentSeatCount: 10,
+      },
+    ];
+  }
 
-    @Get(':concertId/avaliable-dates')
-    async getAvailableDates(
-        @Param('concertId') concertId: number
-    ) {
-        return [
-            {
-                concertEventId: 1,
-                startDate: new Date(),
-                maxSeatCapacity: 50,
-                currentSeatCount: 10,
-            }
-        ]
-    }
+  @Get(":concertEventId/avaliable-seats")
+  async getAvailableSeats(@Param("concertEventId") concertEventId: number) {
+    return [
+      {
+        seatsId: 1,
+        seatNumber: 1,
+      },
+      {
+        seatsId: 2,
+        seatNumber: 2,
+      },
+    ];
+  }
 
-    @Get(':concertEventId/avaliable-seats')
-    async getAvailableSeats(
-        @Param('concertEventId') concertEventId: number
-    ) {
-        return [
-            {
-                seatsId: 1,
-                seatNumber: 1,
-            },
-            {
-                seatsId: 2,
-                seatNumber: 2,
-            }
-        ]
-    }
-
-    @Post('seats/:seatsId/assign')
-    async assignSeat(
-        @Param('seatsId') seatsId: number,
-        @Body('concertEventId') concertEventId: string
-    ) {
-        return true
-    }
+  @Post("seats/:seatsId/assign")
+  async assignSeat(
+    @Param("seatsId") seatsId: number,
+    @Body("concertEventId") concertEventId: string
+  ) {
+    return true;
+  }
 }
