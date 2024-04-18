a71af5f1ed54d1d0b6a2facd15f318cab4c7b56d
dlwlsh92 <ljinho92@gmail.com>
2024-04-19 07:04:32 +0900

    fix: test 후 redis flush

--- src/test/integration_test/waitingQueue.spec.ts
+++ src/test/integration_test/waitingQueue.spec.ts
@@ -15,6 +15,7 @@ describe("예약 서비스를 이용할 수 있는 대기열이 정상적으로 
   let tokenManagementService: TokenManagementService;
   let authenticationService: AuthenticationService;
   let testUtil: TestUtil;
+  let redisService: RedisService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -38,17 +39,21 @@ describe("예약 서비스를 이용할 수 있는 대기열이 정상적으로 
     tokenManagementService.validTokenSeconds = 300;
     tokenManagementService.numberPerCycle = 3;
     testUtil = module.get<TestUtil>(TestUtil);
+    redisService = module.get<RedisService>(RedisService);
+  });
+
+  afterAll(async () => {
+    const redis = await redisService.getClient();
+    await redis.flushall();
   });
 
   it("대기열이 정상적으로 작동하는지 확인", async () => {
     const tokens: string[] = [];
-
     // 4개의 토큰을 생성
     for (let i = 0; i < 4; i++) {
       const token = await authenticationService.createToken();
       tokens.push(token);
     }
-
     /**
      * 현재 사이클을 3으로 설정하였으므로, 3개의 토큰은 사용 가능한 상태이고, 4번째 토큰은 대기열에 들어가야 합니다.
      * */
@@ -57,6 +62,8 @@ describe("예약 서비스를 이용할 수 있는 대기열이 정상적으로 
 
     const result1 = await authenticationService.validateToken(firstToken);
     const result2 = await authenticationService.validateToken(lastToken);
+
+    console.log(result2);
 
     expect(result1.status).toBe("available");
     expect(result2.status).toBe("pending");
