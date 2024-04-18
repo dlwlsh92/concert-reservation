4c1f9beffcfb2dfe5947500a140742de7cbd10ce
dlwlsh92 <ljinho92@gmail.com>
2024-04-19 07:35:31 +0900

    add: 동일한 좌석 동시성 통합테스트 작성

---
+++ src/test/integration_test/concurrentReservation.spec.ts
@@ -0,0 +1,61 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { ReservationService } from "../../domain/reservations/application/reservation.service";
+import { IConcertDetailsReaderToken } from "../../domain/reservations/repositories/concert-details-reader.interface";
+import { ConcertDetailsReaderRepository } from "../../domain/reservations/infrastructure/persistence/concert-details-reader.repository";
+import { IReservationWriteToken } from "../../domain/reservations/repositories/reservation-write.interface";
+import { ReservationWriteRepository } from "../../domain/reservations/infrastructure/persistence/reservation-write.repository";
+import { PrismaService } from "../../database/prisma/prisma.service";
+import { TestUtil } from "./util";
+
+describe("좌석 예약 관련 테스트", () => {
+  let reservationService: ReservationService;
+  let testUtil: TestUtil;
+
+  beforeAll(async () => {
+    let module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ReservationService,
+        {
+          provide: IConcertDetailsReaderToken,
+          useClass: ConcertDetailsReaderRepository,
+        },
+        {
+          provide: IReservationWriteToken,
+          useClass: ReservationWriteRepository,
+        },
+        PrismaService,
+        TestUtil,
+      ],
+    }).compile();
+    reservationService = module.get<ReservationService>(ReservationService);
+    testUtil = module.get<TestUtil>(TestUtil);
+  });
+
+  it("동일한 좌석은 한 사람만 예약할 수 있다.", async () => {
+    const concert = await testUtil.createConcert();
+    const concertEvent = await testUtil.createConcertEvent(concert.id, 3);
+
+    const tenUsers = Array.from({ length: 10 }).map(async (_, index) => {
+      return testUtil.createUser();
+    });
+    const users = await Promise.all(tenUsers);
+    const seatsPromise = Array.from({
+      length: concertEvent.maxSeatCapacity,
+    }).map(async (_, index) => {
+      return testUtil.createSeat(concertEvent.id, index + 1);
+    });
+    const seats = await Promise.all(seatsPromise);
+    const reservationSeatPromise = users.map(async (user, index) => {
+      return reservationService.reserveSeat(
+        seats[0].id,
+        concertEvent.id,
+        user.id
+      );
+    });
+    const reservationSeats = await Promise.allSettled(reservationSeatPromise);
+    const successReservations = reservationSeats.filter(
+      (result) => result.status === "fulfilled"
+    );
+    expect(successReservations.length).toBe(1);
+  });
+});
--- src/test/integration_test/util.ts
+++ src/test/integration_test/util.ts
@@ -1,5 +1,6 @@
 import { PrismaService } from "../../database/prisma/prisma.service";
 import { Injectable } from "@nestjs/common";
+import { addHoursToCurrentTime } from "../utils";
 
 @Injectable()
 export class TestUtil {
@@ -19,6 +20,49 @@ export class TestUtil {
     return this.prisma.user.delete({
       where: {
         id: userId,
+      },
+    });
+  }
+
+  async createConcert() {
+    return this.prisma.concert.create({
+      data: {
+        concertName: "test",
+      },
+    });
+  }
+
+  async createConcertEvent(concertId: number, maxSeatCapacity: number) {
+    return this.prisma.concertEvent.create({
+      data: {
+        concertId: concertId,
+        startDate: addHoursToCurrentTime(10),
+        reservationDate: addHoursToCurrentTime(-1),
+        maxSeatCapacity: maxSeatCapacity,
+      },
+    });
+  }
+
+  async createSeats(concertEventId: number, maxCapacity: number) {
+    return this.prisma.seat.createMany({
+      data: Array.from({ length: maxCapacity }).map((_, index) => ({
+        concertEventId: concertEventId,
+        seatNumber: index + 1,
+        expirationDate: addHoursToCurrentTime(-1),
+        isPaid: false,
+        price: 10000,
+      })),
+    });
+  }
+
+  async createSeat(concertEventId: number, seatNumber: number) {
+    return this.prisma.seat.create({
+      data: {
+        concertEventId: concertEventId,
+        seatNumber: seatNumber,
+        expirationDate: addHoursToCurrentTime(-1),
+        isPaid: false,
+        price: 10000,
       },
     });
   }
