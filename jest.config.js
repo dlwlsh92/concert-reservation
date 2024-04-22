ea4bf4a1c2af38914c040db5dff2270ce0ecae3a
dlwlsh92 <ljinho92@gmail.com>
2024-04-22 23:44:19 +0900

    feat: 서버 환경 분리

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+
+// 환경 변수 설정을 불러옵니다.
+dotenv.config({ path: "./env/.env.dev" });
+
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  verbose: true,
+  testEnvironment: "node",
+};
